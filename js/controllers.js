ebfc6746d214e3cc343b875708705c39513af09b
Angela <agu@hearsaycorp.com>
2014-10-05 11:25:21 -0700

    alignment and text changes

--- js/controllers.js
+++ js/controllers.js
@@ -91,16 +91,16 @@ angular.module('raw.controllers', [])
       $scope.loading = false;
 
       var num_of_items = $scope.metadata.length;
-      var text_width = 169;
+      var text_width = 161;
       var last_width = $scope.titles.width() - text_width * (num_of_items - 1);
 
       for (var i = 0; i < num_of_items; i++) {
         if ((num_of_items - i) == 1) {
-          $scope.titles.append("<input type='text' ng-model='column" + i + "' id='header-" + i + "' style='width:" + last_width + "px;border:1px solid #ccc;margin-bottom:5px;' placeholder='Enter column Title'>");
+          $scope.titles.append("<input type='text' ng-model='column" + i + "' id='header-" + i + "' style='width:" + last_width + "px;border:1px solid #ccc;margin-bottom:5px;' placeholder='Column label'>");
           $scope.metaDataOptions.append("<div style='display:inline-block;width:" + last_width + "px;'><select style='width:100%;'><option>Independent</option><option>Dependent</option></select>\
             <select style='width:100%;'><option value='' disabled selected>Select your option</option><option>Continuous</option><option>Categorical</option></select><div>");
         } else {
-          $scope.titles.append("<input type='text' ng-model='column" + i + "' id='header-" + i + "' style='width:" + text_width + "px;border:1px solid #ccc;margin-bottom:5px;' placeholder='Enter column Title'>");
+          $scope.titles.append("<input type='text' ng-model='column" + i + "' id='header-" + i + "' style='width:" + text_width + "px;border:1px solid #ccc;margin-bottom:5px;' placeholder='Column label'>");
           $scope.metaDataOptions.append("<div style='display:inline-block;width:" + text_width + "px;'><select style='width:100%;'><option>Independent</option><option>Dependent</option></select>\
             <select style='width:100%;'><option value='' disabled selected>Select your option</option><option>Continuous</option><option>Categorical</option></select><div>");
         }
